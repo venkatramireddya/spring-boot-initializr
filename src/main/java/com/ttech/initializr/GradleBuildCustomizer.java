f6d0a9ab33d1b152fa4654b2afb699cc2d73cf7d
Venkat Ambati <venkatrami.ambati@gmail.com>
2021-07-05 21:01:04 -0500

    refactor

--- src/main/java/com/ttech/initializr/GradleBuildCustomizer.java
+++ src/main/java/com/ttech/initializr/GradleBuildCustomizer.java
@@ -10,7 +10,7 @@ public class GradleBuildCustomizer implements BuildCustomizer<GradleBuild> {
 	public void customize(GradleBuild build) {
 		
 		
-		MavenRepository repo = MavenRepository.withIdAndUrl("hcl-maven-central", "https://repo.hcl.maven.com/maven").name("hcl-maven-central").build();
+		MavenRepository repo = MavenRepository.withIdAndUrl("ttech-maven-central", "https://repo.ttech.maven.com/maven").name("ttech-maven-central").build();
 		build.repositories().add(repo);
 		//build.repositories().remove("maven-central");		
 	}
